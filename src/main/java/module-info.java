9ed1506a9eecb4a445760427acb5b915a5548f98
RoanLaanen <69250260+RoanLaanen@users.noreply.github.com>
2023-12-14 18:46:31 +0100

    fix modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.verticalslice {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires mysql.connector.j;
 
 
     opens com.example.verticalslice to javafx.fxml;
