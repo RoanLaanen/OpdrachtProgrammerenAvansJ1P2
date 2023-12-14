e7e711e92a9fe5a5240d2911512a271e8e1ccb69
Dymo Waltheer <dymokilan@gmail.com>
2023-12-14 21:33:42 +0100

    Refactor and simplify GUI by removing commented-out code
    This commit removes bulky, commented-out code, and also refactors the existing codes. Hard-coded titles and labels have been replaced by dynamic ones fetched from the 'DatabaseConnection' class. Additionally, unnecessary spaces and alignments have also been removed for readability and simplicity.

--- src/main/java/com/example/verticalslice/GUI.java
+++ src/main/java/com/example/verticalslice/GUI.java
@@ -19,64 +19,21 @@ public class GUI extends Application {
 
     @Override
     public void start(Stage stage) {
+        stage.setTitle(DatabaseConnection.cursusArray.get(0).getCursusNaam());
 
-// ---------------------------------------------------------------
-//        Label title = new Label(DatabaseConnection.cursusArray.get(0).getCursusNaam());
-//            title.setStyle("-fx-font-size: 20px; -fx-font-weight: bold;");
-//
-//        HBox titleHBox = new HBox(title);
-//            titleHBox.setPrefHeight(50);
-//            titleHBox.setPrefWidth(480);
-//            titleHBox.setAlignment(Pos.CENTER);
-////            titleHBox.setStyle("-fx-background-color: #A8D1D1;");
-////        mainPane.setStyle("-fx-background-color: #9EA1D4;");
-//        Label onderwerp = new Label(DatabaseConnection.cursusArray.get(0).getOnderwerp());
-//        Label niveau = new Label(DatabaseConnection.cursusArray.get(0).getNiveau());
-//        Text introductieTekst = new Text(DatabaseConnection.cursusArray.get(0).getIntroductieTekst());
-//
-//        introductieTekst.setWrappingWidth(480);
-//        HBox onderwerpHBox = new HBox(onderwerp);
-//            onderwerpHBox.setAlignment(Pos.CENTER);
-//            onderwerpHBox.setPrefWidth(240);
-////            onderwerpHBox.setStyle("-fx-background-color: #D8D1D1;");
-//
-//        HBox niveauHBox = new HBox(niveau);
-//            niveauHBox.setAlignment(Pos.CENTER);
-//            niveauHBox.setPrefWidth(240);
-////            niveauHBox.setStyle("-fx-background-color: #F8D3A1;");
-//
-//        HBox row2 = new HBox(onderwerpHBox, niveauHBox);
-//            row2.setPrefWidth(480);
-//            row2.setAlignment(Pos.CENTER);
-////            row2.setStyle("-fx-background-color: #A8D1D1;");
-//        HBox row3 = new HBox(introductieTekst);
-////            row3.setStyle("-fx-background-color: #B8D1D1;");
-//            row3.setPrefHeight(100);
-//            row3.setMaxWidth(480);
-//
-//        VBox rows = new VBox(10, titleHBox, row2, row3);
-//        StackPane.setMargin(rows, new Insets(10));
-// ---------------------------------------------------------------
-
-        stage.setTitle("BorderPane");
-
-        ComboBox<String> comboBox = new ComboBox<>(FXCollections.observableArrayList("Course1"));
+        ComboBox<String> comboBox = new ComboBox<>(FXCollections.observableArrayList(DatabaseConnection.cursusArray.get(0).getCursusNaam()));
         comboBox.getSelectionModel().selectFirst();
         comboBox.prefWidthProperty().bind(stage.widthProperty().divide(3));
-
-        //comboBox.getItems().add("Option 4");
-
-
 
         HBox dropdownContainer = new HBox(comboBox);
             dropdownContainer.setAlignment(Pos.CENTER);
             dropdownContainer.setPrefHeight(50);
 
-        Label title = new Label("Course1");
+        Label title = new Label(DatabaseConnection.cursusArray.get(0).getCursusNaam());
             title.setStyle("-fx-font-size: 20px; -fx-font-weight: bold;");
-        Label onderwerp = new Label("Onderwerp");
+        Label onderwerp = new Label(DatabaseConnection.cursusArray.get(0).getOnderwerp());
         onderwerp.setStyle("-fx-font-size: 14px; -fx-font-weight: bold;");
-        Label niveau = new Label("Niveau");
+        Label niveau = new Label(DatabaseConnection.cursusArray.get(0).getNiveau());
             niveau.setStyle("-fx-font-size: 14px; -fx-font-weight: bold;");
         Text introductieTekst = new Text(DatabaseConnection.cursusArray.get(0).getIntroductieTekst());
             introductieTekst.setStyle("-fx-font-size: 14px;");
@@ -102,14 +59,6 @@ public class GUI extends Application {
 
 
         VBox mainContent = new VBox(titleContainer, dualContainer, introductieTekstContainer);
-
-
-
-
-
-
-
-
 
         Button addButton = new Button("Add");
             HBox.setHgrow(addButton, Priority.ALWAYS);
@@ -146,27 +95,14 @@ public class GUI extends Application {
 
         });
 
-
-
-
-
         HBox buttonContainer = new HBox(addButton, editButton, deleteButton);
             buttonContainer.setPrefHeight(50);
             buttonContainer.setAlignment(Pos.CENTER);
-
-
-
-
-
 
         // create a BorderPane
         BorderPane border_pane = new BorderPane(mainContent,
                 dropdownContainer, null, buttonContainer, null);
         border_pane.setStyle("-fx-background-color: #FAF9F6;");
-
-        // set alignment
-        border_pane.setAlignment(dropdownContainer, Pos.CENTER);
-        border_pane.setAlignment(buttonContainer, Pos.CENTER);
 
         Scene scene = new Scene(border_pane, 720, 480);
         stage.setScene(scene);
