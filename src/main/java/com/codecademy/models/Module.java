44e0eed36d0d869a21dc8e646a6ba18e7c9e2b66
Mech <stefrensma@gmail.com>
2024-01-05 20:21:54 +0100

    added Webcast and Module class

---
+++ src/main/java/com/codecademy/models/Module.java
@@ -0,0 +1,49 @@
+package com.codecademy.models;
+
+public class Module {
+    private String title;
+    private float version;
+    private String description;
+    private String emailContact;
+    private String nameContact;
+    private int contentID;
+    private String courseName;
+    public Module(String title,float version, String description,String emailContact,String nameContact,int contentID,String courseName){
+        this.title = title;
+        this.version = version;
+        this.description = description;
+        this.emailContact = emailContact;
+        this.nameContact = nameContact;
+        this.contentID = contentID;
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public float getVersion() {
+        return version;
+    }
+
+    public int getContentID() {
+        return contentID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getEmailContact() {
+        return emailContact;
+    }
+
+    public String getNameContact() {
+        return nameContact;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
+
---
+++ src/main/java/com/codecademy/models/Webcast.java
@@ -0,0 +1,48 @@
+package com.codecademy.models;
+
+public class Webcast {
+    private String title;
+    private String description;
+    private String nameLecturer;
+    private String organisationLecturer;
+    private String url;
+    private int duration;
+    private int contentID;
+    public Webcast(String title,int duration, String description,String nameLecturer,String organisationLecturer,int contentID,String url){
+        this.title = title;
+        this.duration = duration;
+        this.description = description;
+        this.nameLecturer = nameLecturer;
+        this.organisationLecturer = organisationLecturer;
+        this.contentID = contentID;
+        this.url = url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getContentID() {
+        return contentID;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public String getNameLecturer() {
+        return nameLecturer;
+    }
+
+    public String getOrganisationLecturer() {
+        return organisationLecturer;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
