4304614345cd6a5b1d482ff2136d163db50ba40b
Mech <stefrensma@gmail.com>
2024-01-09 21:49:23 +0100

    Added userCourses class

---
+++ src/main/java/com/codecademy/controllers/UserCoursesController.java
@@ -0,0 +1,18 @@
+package com.codecademy.controllers;
+
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UserCoursesController implements Initializable {
+    private Stage primaryStage;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) { }
+
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+}
