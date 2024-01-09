bcd8effdd6f866a609db7399feefa7248437200e
Mech <stefrensma@gmail.com>
2024-01-09 22:43:47 +0100

    Added css folder for improved project structure. Added some design in the Users class and added a back arrow which works

--- src/main/java/com/codecademy/controllers/UsersController.java
+++ src/main/java/com/codecademy/controllers/UsersController.java
@@ -1,12 +1,17 @@
 package com.codecademy.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
-import javafx.scene.input.ContextMenuEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -33,5 +38,15 @@ public class UsersController implements Initializable {
     }
 
     public void selectItem() {
+    }
+
+    public void changeSceneToMain(MouseEvent mouseEvent)
+        throws IOException {
+            Parent parent = FXMLLoader.load(getClass().getResource("/MainWindow.fxml"));
+            Scene scene = new Scene(parent);
+
+            Stage window = (Stage) ((Node)mouseEvent.getSource()).getScene().getWindow();
+            window.setScene(scene);
+            window.show();
     }
 }
