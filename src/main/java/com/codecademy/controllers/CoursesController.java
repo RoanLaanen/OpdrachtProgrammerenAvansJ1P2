13b69ab627f8caf6374fb9dc5cdfc7993392ef2a
Mech <stefrensma@gmail.com>
2024-01-10 00:01:11 +0100

    Cleaned up most code from Courses.fxml and added the Courses.css file. Added some design for Courses

--- src/main/java/com/codecademy/controllers/CoursesController.java
+++ src/main/java/com/codecademy/controllers/CoursesController.java
@@ -1,9 +1,15 @@
 package com.codecademy.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -18,4 +24,13 @@ public class CoursesController implements Initializable {
         this.primaryStage = primaryStage;
     }
 
+    public void changeSceneToMain(MouseEvent mouseEvent) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("/MainWindow.fxml"));
+        Scene scene = new Scene(parent);
+
+        Stage window = (Stage) ((Node)mouseEvent.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+
+    }
 }
