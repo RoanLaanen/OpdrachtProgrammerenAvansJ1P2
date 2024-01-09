c98351aa617a98e41f5f6c2b9edfa28fede4f8db
Mech <stefrensma@gmail.com>
2024-01-09 21:16:20 +0100

    Added users scene.

---
+++ src/main/java/com/codecademy/controllers/UsersController.java
@@ -0,0 +1,20 @@
+package com.codecademy.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class UsersController implements Initializable {
+    private Stage primaryStage;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) { }
+
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+}
