403d2896693b8deef6eed8f96a6b2105733ae7ca
RoanLaanen <69250260+RoanLaanen@users.noreply.github.com>
2024-01-12 14:01:00 +0100

    Cleaned up code and removed unused methods and variables

--- src/main/java/com/codecademy/database/DatabaseConnection.java
+++ src/main/java/com/codecademy/database/DatabaseConnection.java
@@ -1,8 +1,6 @@
 package com.codecademy.database;
 
-import com.codecademy.models.Course;
 import com.codecademy.models.User;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -11,30 +9,21 @@ import java.sql.PreparedStatement;
 import java.util.ArrayList;
 
 public class DatabaseConnection {
-    private static ArrayList<Course> cursusArray = new ArrayList<>();
-    private static ArrayList<String> cursusNaamArray = new ArrayList<>();
     private static final String connectionUrl = "jdbc:sqlserver://codecademy.database.windows.net:1433;database=codecademyData;user=groep5@codecademy;password=AvansBreda123;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30";
     private static Connection con = null;
     private static Statement stmt = null;
     private static ResultSet rs = null;
 
-
-
     public static ArrayList<User> getAllUsers() {
-
         ArrayList<User> users = new ArrayList<>();
-
         try {
-
             con = DriverManager.getConnection(connectionUrl);
             String SQL = "SELECT * FROM [User]";
             stmt = con.createStatement();
             rs = stmt.executeQuery(SQL);
-
             while (rs.next()) {
                 users.add(new User(rs.getString("name"), rs.getString("email"), rs.getString("dateOfBirth"), rs.getString("isMale"), rs.getString("address"), rs.getString("zip"), rs.getString("country")));
             }
-
         } catch (Exception e) {
             System.out.println("Error: " + e);
         } finally {
@@ -43,12 +32,12 @@ public class DatabaseConnection {
             } catch (Exception e) {
                 System.out.println("Error: " + e);
             }
-            closeConnection(con, stmt);
+            closeConnection(con);
         }
         return users;
     }
 
-    public static void addUser (User user) {
+    public static void addUser(User user) {
         try {
             con = DriverManager.getConnection(connectionUrl);
             String SQL = "INSERT INTO [User](name, email, dateOfBirth, isMale, address, zip, country) VALUES (?, ?, ?, ?, ?, ?, ?)";
@@ -65,7 +54,7 @@ public class DatabaseConnection {
         } catch (Exception e) {
             System.out.println("Error: " + e);
         } finally {
-            closeConnection(con, stmt);
+            closeConnection(con);
         }
     }
 
@@ -87,7 +76,7 @@ public class DatabaseConnection {
         } catch (Exception e) {
             System.out.println("Error: " + e);
         } finally {
-            closeConnection(con, stmt);
+            closeConnection(con);
         }
     }
 
@@ -102,59 +91,11 @@ public class DatabaseConnection {
         } catch (Exception e) {
             System.out.println("Error: " + e);
         } finally {
-            closeConnection(con, stmt);
+            closeConnection(con);
         }
     }
 
-
-
-
-    public static void addCursus(String courseName, String topic, String introText, String level) {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection(connectionUrl);
-            String SQL = "INSERT INTO course (courseName, topic, introText, level) VALUES ('" + courseName + "', '" + topic + "', '" + introText + "', '" + level + "')";
-            stmt = con.createStatement();
-            stmt.executeUpdate(SQL);
-        } catch (Exception e) {
-            System.out.println("Error: " + e);
-        } finally {
-            closeConnection(con, stmt);
-        }
-    }
-
-    public static void deleteCursus(String courseName) {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection(connectionUrl);
-            String SQL = "DELETE FROM Cursus WHERE courseName = '" + courseName + "'";
-            stmt = con.createStatement();
-            stmt.executeUpdate(SQL);
-        } catch (Exception e) {
-            System.out.println("Error: " + e);
-        } finally {
-            closeConnection(con, stmt);
-        }
-    }
-
-    public static void updateCursus(String oldCursusNaam, String courseName, String topic, String introText, String level) {
-        try {
-
-            cursusArray.clear();
-            cursusNaamArray.clear();
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection(connectionUrl);
-            String SQL = "UPDATE Cursus SET courseName = '" + courseName + "', topic = '" + topic + "', introText = '" + introText + "', level = '" + level + "' WHERE courseName = '" + oldCursusNaam + "'";
-            stmt = con.createStatement();
-            stmt.executeUpdate(SQL);
-        } catch (Exception e) {
-            System.out.println("Error: " + e);
-        } finally {
-            closeConnection(con, stmt);
-        }
-    }
-
-    private static void closeConnection(Connection con, Statement stmt) {
+    private static void closeConnection(Connection con) {
         if (stmt != null) try {
             stmt.close();
         } catch (Exception exception) {
@@ -166,8 +107,6 @@ public class DatabaseConnection {
             System.out.println("Error: " + exception);
         }
     }
-
-
 }
 
 
